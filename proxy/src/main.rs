dbac375df29423813b7badb484f1fce49e4d2514
Richard Leek <richard@richardleek.com>
2024-07-13 19:28:52 +0200

    cleanup proxy code

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -3,7 +3,7 @@ use futures_util::{SinkExt, StreamExt};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
-    sync::mpsc::{self, UnboundedReceiver},
+    sync::mpsc::{self, UnboundedReceiver, UnboundedSender},
 };
 use tokio_tungstenite::{accept_async, tungstenite::Message, WebSocketStream};
 
@@ -28,52 +28,57 @@ async fn accept_connection(stream: TcpStream) {
         .await
         .expect("Error during the websocket handshake occurred");
 
+    println!("Websocket connection accepted");
+
     let (tx, rx) = mpsc::unbounded_channel::<Vec<u8>>();
 
     let mut ws = WS::new(websocket, rx);
 
     loop {
         tokio::select! {
-                    Some(message) = ws.socket.next() => match message {
-                        Ok(Message::Binary(buf)) => {
-                            let _ = eosocket.write(&buf).await;
-                        }
-                        _ => {}
-                    },
-                    _ = async {
-        let mut length_buf = vec![0; 2];
-                    match eosocket.read(&mut length_buf).await {
-                        Ok(bytes_read) => {
-                            if bytes_read == 0 {
-                            return;
-                            }
-                        }
-                        Err(_) => {}
+            Some(message) = ws.socket.next() => match message {
+                Ok(Message::Binary(buf)) => {
+                    if let Err(_) = eosocket.write(&buf).await {
+                        // Client disconnected
+                        break;
                     }
-
-                    let length = decode_number(&length_buf);
-                    if length != 0 {
-                        let mut buf = vec![0; length as usize];
-                        match eosocket.read(&mut buf).await {
-                            Ok(bytes_read) => {
-                                if bytes_read == 0 {
-                            return;
-                                }
-                            }
-                            Err(_) => {}
-                        }
-
-                        println!("Recv: {:?}", buf);
-
-                        let _ = tx.send(buf);
-                    }
-                        } => {}
-            Some(buf) = ws.rx.recv() => {
-                println!("Sending: {:?}", buf);
-                let _ = ws.socket.send(Message::Binary(buf)).await;
-            }
                 }
+                _ => {}
+            },
+            result = read_eo_packet(&mut eosocket, &tx) => match result {
+                    Ok(Some(bytes_read)) => {
+                        if bytes_read == 0 {
+                            // Server disconnected
+                            break;
+                        }
+                    }
+                    _ => {}
+                },
+            Some(buf) = ws.rx.recv() => {
+                if let Err(_) = ws.socket.send(Message::Binary(buf)).await {
+                    // Client disconnected
+                    break;
+                }
+            }
+        }
     }
+
+    println!("Websocket connection dropped");
+}
+
+async fn read_eo_packet(
+    eosocket: &mut TcpStream,
+    tx: &UnboundedSender<Vec<u8>>,
+) -> Result<Option<usize>, Box<dyn std::error::Error + Send + Sync>> {
+    let mut length_buf = vec![0; 2];
+    eosocket.read(&mut length_buf).await?;
+
+    let length = decode_number(&length_buf);
+    let mut buf = vec![0; length as usize];
+    let bytes_read = eosocket.read(&mut buf).await?;
+
+    let _ = tx.send(buf);
+    Ok(Some(bytes_read))
 }
 
 struct WS {
